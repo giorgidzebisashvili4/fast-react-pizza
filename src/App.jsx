d226011e31d1db95d460883efefae80323c484ad
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2025-01-07 15:54:07 +0400

    added applayout and browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home.jsx";
+import Menu from "./features/menu/Menu.jsx";
+import Cart from "./features/cart/Cart.jsx";
+import CreateOrder from "./features/order/CreateOrder.jsx";
+import Order from "./features/order/Order.jsx";
+import Error from "./ui/Error.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
 function App() {
-  return <div>Hello VIte!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
