34f3d829da8a36aaa798d0c7e58f68a30a242d72
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2025-01-09 11:38:03 +0400

    implement order api fetching and diplaying it

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./ui/Home.jsx";
 import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
-import Order from "./features/order/Order.jsx";
+import Order, { loader as orderLoader } from "./features/order/Order.jsx";
 import Error from "./ui/Error.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
@@ -32,6 +33,8 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
