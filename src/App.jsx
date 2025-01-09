580d4fcab097a5440b952f26d952bb2d01479a1a
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2025-01-09 10:14:51 +0400

    turn of eslint features that was cousing problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home.jsx";
-import Menu from "./features/menu/Menu.jsx";
+import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
 import Order from "./features/order/Order.jsx";
@@ -19,6 +19,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: menuLoader,
         errorElement: <Error />,
       },
       {
